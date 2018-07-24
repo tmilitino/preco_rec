cf4a8f13124405dee89deeb1af5fd688cf5e5a9b
tmilitino <tmilitino.tm@gmail.com>
2018-07-24 16:35:09 -0300

    w6

--- prod.js
+++ prod.js
@@ -1,7 +1,15 @@
 var app = require('./config/express')();
-var rotasProdutos = require('./app/routes/produtos')(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('io',io);
 
 var porta = process.env.PORT || 3000;
-http.listen(porta,function(){
-    console.log("servidor rodando");
-}); 
+var server = http.listen(porta, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
