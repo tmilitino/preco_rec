8649aca509e202fce1903bfe5a4942a8407bbc48
TMilithus <tmilithus@hosT2m.milithus>
2018-07-24 22:53:20 -0300

    w8

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
 
         connection.query('select * from produto', function(err, results){
             console.log(err);
-            res.send(results);
+            res.render('produtos/lista',{lista:results});
 
         });
 
