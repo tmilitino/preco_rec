ef7e6b4c4e9da375470ac48ff520cd39e81697c9
TMilithus <tmilithus@hosT2m.milithus>
2018-07-25 22:10:43 -0300

    w10

---
+++ app/infra/dbConect.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+module.exports = function(){
+mysql.createConnection({
+    host: "us-cdbr-iron-east-04.cleardb.net",
+    user: "b67f328959284e",
+    password : "e68f1cf4",
+    database: "heroku_d2e6481d2ce58ab",
+});
+}
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,20 +1,13 @@
+var dbConect = require('../infra/dbConect');
+
 module.exports = function(app) {
-    app.get("/produtos",function(req, res) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: "us-cdbr-iron-east-04.cleardb.net",
-            user: "b67f328959284e",
-            password : "e68f1cf4",
-            database: "heroku_d2e6481d2ce58ab",
-        });
+    app.get("/produtos",function(req, res) {        
+        var connection = dbConect();
 
         connection.query('select * from produto', function(err, results){
             console.log(err);
             res.render('produtos/lista',{lista:results});
-
         });
-
         connection.end();
-
     });
 }
