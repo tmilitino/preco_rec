e6c799ef830542a3d0b0b659ba18dfae83ae59b4
tmilitino <tmilitino.tm@gmail.com>
2018-07-26 09:55:06 -0300

    volta w13

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -4,19 +4,12 @@ module.exports = function(app) {
     app.get("/produtos",function(req, res) {
 
         var connection = dbConect();
-        var cons = "1";
-        if (cons = ""){
+        
         connection.query('select * from produto', function(err, results){
             console.log(err);
             res.render('produtos/lista',{lista:results});
 
-        });}
-        else {
-          connection.query('select * from produto where id like %'+cons+'% or nome like %'+cons+'% or ncm like %+'cons'+%', function(err, results){
-              console.log(err);
-              res.render('produtos/lista',{lista:results});
-              });
-        }
+        });
 
         connection.end();
 
