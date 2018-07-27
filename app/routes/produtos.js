afdf23f83343335c3536a6ad12c207ee2b812eaf
tmilitino <tmilitino.tm@gmail.com>
2018-07-27 11:01:06 -0300

    w18.1

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
     app.get("/produtos",function(req, res) {
 
         var connection = app.infra.dbConect();
-        var produtosBanco = new app.infra.produtosDAO(connection)
+        var produtosBanco = new app.infra.ProdutosDAO(connection)
          produtosBanco.lista(function(err, results){
             console.log(err);
             res.render('produtos/lista',{lista:results});
