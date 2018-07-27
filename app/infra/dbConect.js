9ca5e0b3cfb4866947363601c0d834804cbb9597
tmilitino <tmilitino.tm@gmail.com>
2018-07-27 10:19:53 -0300

    w17

--- app/infra/dbConect.js
+++ app/infra/dbConect.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql');
-module.exports = function(){
+function dbConect(){
 return mysql.createConnection({
     host: "us-cdbr-iron-east-04.cleardb.net",
     user: "b67f328959284e",
     password : "e68f1cf4",
     database: "heroku_d2e6481d2ce58ab",
-});
+});}
 
-}   
+module.exports = function(){
+  return dbConect
+}
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,10 +1,8 @@
-var dbConect = require('../infra/dbConect');
-
 module.exports = function(app) {
     app.get("/produtos",function(req, res) {
 
-        var connection = dbConect();
-        
+        var connection = app.infra.dbConect();
+
         connection.query('select * from produto', function(err, results){
             console.log(err);
             res.render('produtos/lista',{lista:results});
